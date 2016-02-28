73ece6d621aadfd389160ee7b1a85a9d07ddb221
Laze_pc <lpancevski@gmail.com>
2016-02-28 16:23:46 +0100

    - updates

--- Day 2/navigation-drawer/src/main/java/com/example/course/navigation_drawer/OpertingSystemFragment.java
+++ Day 2/navigation-drawer/src/main/java/com/example/course/navigation_drawer/OpertingSystemFragment.java
@@ -10,19 +10,19 @@ import android.widget.TextView;
 public class OpertingSystemFragment extends Fragment {
 	
 	  public static final String ARG_OS = "OS";
-	  private int intValue;
+	  private String value;
 
 	  @Override
 	  public View onCreateView(LayoutInflater inflater, ViewGroup container,
 		  Bundle savedInstanceState) {
 		  View view = inflater.inflate(R.layout.fragment_layout, null);
 		  TextView textView = (TextView) view.findViewById(R.id.textView1);
-		  textView.setText("" + intValue);
+		  textView.setText(" " + value);
 		  return view;
 	  }
 
 	  @Override
 	  public void setArguments(Bundle args) {
-		intValue = args.getInt(ARG_OS);
+		value = args.getString(ARG_OS);
 	  }
 }
